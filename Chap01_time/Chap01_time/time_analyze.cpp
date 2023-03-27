7ac82e4b6f5efb7e22edc5ac018158b993a2165d
dmstnddl <eunjin1sang@naver.com>
2023-03-27 14:33:10 +0900

    chap01 time added

--- Chap01_time/Chap01_time/time_analyze.cpp
+++ Chap01_time/Chap01_time/time_analyze.cpp
@@ -2,12 +2,27 @@
 #include <cstdlib>
 #include <ctime>
 
+int sum_of_N(int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n + 1; i++) {
+		sum = sum + i;
+	}
+	return sum;
+}
+
+int sum_of_N2(int n)
+{
+	return(n * (n + 1)) / 2;
+}
+
 int main()
 {
 	clock_t start, finish;
 	double duration;
 
 	start = clock();
+	int sum = sum_of_N(100000000);
 
 	int a = 10 + 20;
 
